21e6dec6f12a7fc41f2d9bad3e8e59e762dc4900
tukumo0411 <pianosonata0308@icloud.com>
2022-06-21 14:56:49 +0900

    テキスト97Pまで

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,12 +8,25 @@
    </style>
 </head>
 <body>
-    <h1>Blade/Index</h1>
-    <p>&#064;foreachディレクティブの例</p>
-    <ol>
-    @foreach($data as $item)
-    <li>{{$item}}
-    @endforeach
+    @extends('layouts.helloapp')
+
+    @section('title', 'Index')
+    
+    @section('menubar')
+       @parent
+       インデックスページ
+    @endsection
+    
+    @section('content')
+    <p>ここが本文のコンテンツです。</p>
+    <ul>
+    @each('components.item', $data, 'item')
+    </ul>
+ @endsection
+ 
+    @section('footer')
+    copyright 2020 tuyano.
+    @endsection
     </ol>
  </body>
 </html>
