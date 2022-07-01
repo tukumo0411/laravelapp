f260cf5c2889b49842329b9167b8a2fb72b2b08e
tukumo0411 <pianosonata0308@icloud.com>
2022-07-01 14:25:55 +0900

    テキストP199  DB::updateによる更新

--- routes/web.php
+++ routes/web.php
@@ -23,19 +23,27 @@ use App\Http\Middleware\HelloMiddleware;
 Route::get('hello', 'HelloController@index')
    ->middleware(HelloMiddleware::class);
 
+//実習
 Route::get('jissyu2', 'JissyuController@index');
 Route::get('jissyu3', 'Jissyu3_1Controller@index');
 Route::post('jissyu3', 'Jissyu3_1Controller@post');
 Route::get('jissyu4', 'Jissyu3_2Controller@index');
 Route::post('jissyu4', 'Jissyu3_2Controller@post');
 
+//効果測定
 Route::get('kouka1_1', 'Kouka1_1Controller@index');
-
 Route::get('kouka1_2', 'Kouka1_2Controller@index');
 Route::post('kouka1_2', 'Kouka1_2Controller@post');
 
+//DB::insertレコード作成
 Route::get('hello/add', 'HelloController@add');
 Route::post('hello/add', 'HelloController@create');
+
+//updateによる更新
+Route::get('hello/edit', 'HelloController@edit');
+Route::post('hello/edit', 'HelloController@update');
+
+
 
 
 
