dd27e626d520f1f590df931434fe7d5c00a4b2d2
tukumo0411 <pianosonata0308@icloud.com>
2022-07-06 12:19:34 +0900

    テキストP253 グローバルスコープ作成

--- app/Person.php
+++ app/Person.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model
 {
@@ -19,6 +20,15 @@ class Person extends Model
     public function scopeAgeLessThan($query, $n)
     {
        return $query->where('age', '<=', $n);
+    }
+
+    protected static function boot()
+    {
+       parent::boot();
+    
+       static::addGlobalScope('age', function (Builder $builder) {
+           $builder->where('age', '>', 20);
+       });
     }
     
 }
