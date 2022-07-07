7d649f5a04f4ad08377bc64504fd743110e35bb1
tukumo0411 <pianosonata0308@icloud.com>
2022-07-07 14:05:40 +0900

    テキストP271～P274 Boardテーブル

--- routes/web.php
+++ routes/web.php
@@ -69,6 +69,12 @@ Route::post('person/edit', 'PersonController@update');
 Route::get('person/del', 'PersonController@delete');
 Route::post('person/del', 'PersonController@remove');
 
+//Boardテーブルの作成
+Route::get('board', 'BoardController@index');
+
+Route::get('board/add', 'BoardController@add');
+Route::post('board/add', 'BoardController@create');
+
 
 
 
