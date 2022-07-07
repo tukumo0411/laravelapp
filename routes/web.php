06509ea3e86bad39fe78ef6d58bb7559ed54f6f4
tukumo0411 <pianosonata0308@icloud.com>
2022-07-07 11:53:05 +0900

    テキストP256~259 モデルの保存・更新・削除

--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,10 @@ Route::get('person', 'PersonController@index');
 Route::get('person/find', 'PersonController@find');
 Route::post('person/find', 'PersonController@search');
 
+//モデルを修正
+Route::get('person/add', 'PersonController@add');
+Route::post('person/add', 'PersonController@create');
+
 
 
 
