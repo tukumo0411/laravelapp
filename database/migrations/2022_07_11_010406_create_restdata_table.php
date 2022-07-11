a00311c4a5c0c147809a364e75ad525f4d00a7a0
tukumo0411 <pianosonata0308@icloud.com>
2022-07-11 10:25:03 +0900

    テキストP296まで RESTの準備

---
+++ database/migrations/2022_07_11_010406_create_restdata_table.php
@@ -0,0 +1,23 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRestdataTable extends Migration
+{
+   public function up()
+   {
+       Schema::create('restdata', 
+               function (Blueprint $table) {
+           $table->increments('id');
+           $table->string('message');
+           $table->string('url');
+           $table->timestamps();
+       });
+   }
+
+   public function down()
+   {
+       Schema::dropIfExists('restdata');
+   }
+}
